9bee917d538e26556d1af6ac5c4e63264e3beeb5
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-08-08 16:03:06 +0530

    Delete Admin.js

--- models/Admin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Admin = void 0;
-var mongoose_1 = require("mongoose");
-var AdminSchema = new mongoose_1.default.Schema({
-    username: String,
-    password: String, // already hashed from Excel
-});
-exports.Admin = mongoose_1.default.models.Admin || mongoose_1.default.model('Admin', AdminSchema);
